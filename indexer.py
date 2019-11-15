ead23f10de3ca99fa08b4f68eafbedcc1bd911b2
misgnome <misgnome@gmail.com>
2019-11-15 15:54:38 -0600

    extract box scores but works

--- indexer.py
+++ indexer.py
@@ -4,9 +4,12 @@ import string
 
 BOX_SELECTOR = CSSSelector(".center a ")
 UNFORMATTED_URL = "https://www.basketball-reference.com{}"
-basic_stats_selector = ss("table", id = lambda x : x and x.endswith('-game-basic'))
+basic_stats_strainer = ss("table", id = lambda x : x and x.endswith('-game-basic'))
 
 def extract_box_scores_from_raw_box_score_pages(authorizer,season):
+    '''
+    takes raw html and returns a list of string matrices 
+    '''
     steve_cur = authorizer.conn.cursor()
     steve_cur.execute("""
     select url,raw_html from raw_box_score_pages where season = %s
@@ -14,17 +17,44 @@ def extract_box_scores_from_raw_box_score_pages(authorizer,season):
 
     box_scores = []
     for url,raw_html in steve_cur:
-        game_soup = bs(raw_html, 'lxml', parse_only = basic_stats_selector)
+        game_soup = bs(raw_html, 'lxml', parse_only = basic_stats_strainer)
         date = url[47:55]
-    game_stats = []
-    for tr in game_soup.find_all('tr'):
-        tds = tr.find_all(['td', 'th'])
-        player_stats = [td.text for td in tds]
-        if player_stats[0] != "Starters" and player_stats[0] != "Reserves" and player_stats[0] != '': 
-            game_stats.append(player_stats)
+
+        game_stats = []
+        scores = []
+        teams = []
+        for table in game_soup.find_all('table'):
+            team = table.get('id')
+            team = team[4:7]
+            teams.append(team)
+            for tr in table.find_all('tr'):
+                tds = tr.find_all(['td', 'th'])
+                player_stats = [td.text for td in tds]
+                if player_stats[0] == "Team Totals":
+                    scores.append(int(player_stats[-2]))
+                if player_stats[0] != "Starters" and player_stats[0] != "Reserves" and player_stats[0] != '': 
+                    player_stats.insert(1, date)
+                    player_stats.insert(1, team)
+                    game_stats.append(player_stats)
+
+        for player in game_stats:
+            i = 0 
+            for team in teams: 
+                if team in player: 
+                    print(i)
+                    string = "L"
+                    if scores[i] > scores[(i+1)%2]:
+                        string = "W"  
+                    print(string)
+                    player.append(string)
+                i += 1
+
+        box_scores.append(game_stats)
+
                 ###
                 pass
     steve_cur.close()
+    return(game_stats)
 
 def extract_box_score_links_from_raw_schedule_pages(authorizer, season):
 
